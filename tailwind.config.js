b33a267ca23df2e9ddbcb0edef064e6645021973
Alishah <zahra.alishah.1996@gmail.com>
2025-08-26 23:52:46 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Vazirmatn"],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'base', // only generate global styles
+      strategy: 'class', // only generate classes
+    }),
+  ],
+};
